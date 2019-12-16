b507b128dd798f1723b3b2ebcb4a6846d19f6e4e
Gabor <orbangabeszb@gmail.com>
2019-12-16 16:03:17 +0100

    input and output stream added

--- src/com/orgabor/Main.java
+++ src/com/orgabor/Main.java
@@ -1,10 +1,22 @@
 package com.orgabor;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try(Socket socket = new Socket("localhost", 5000)) {
+			BufferedReader echoes = new BufferedReader(
+									new InputStreamReader(socket.getInputStream()));
+			PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
+			
+		} catch(IOException e) {
+			System.out.println("Client exception: " + e.getMessage());
+		}
 	}
 
 }
